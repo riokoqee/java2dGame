feec41afbf9f4c406282f431bbaefa3e71b48437
rioko <rioko988@gmail.com>
2025-09-15 23:08:39 +0500

    feat: improved render performance

--- src/object/OBJ_Key.java
+++ src/object/OBJ_Key.java
@@ -1,16 +1,21 @@
 package object;
 
+import main.GamePanel;
+
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class OBJ_Key extends SuperObject{
 
-    public OBJ_Key(){
+    GamePanel gp;
+
+    public OBJ_Key(GamePanel gp) {
 
         name = "Key";
         try {
             image = ImageIO.read(getClass().getResourceAsStream("/objects/key.png"));
+            uTool.scaleImage(image, gp.tileSize, gp.tileSize);
         }catch(IOException e){
             e.printStackTrace();
         }
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,13 +1,12 @@
 package tile;
 
 import main.GamePanel;
+import main.UtilityTool;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 public class TileManager {
     GamePanel gp;
@@ -25,31 +24,23 @@ public class TileManager {
     }
 
     public void getTileImage() {
+            setup(0, "grass", false);
+            setup(1, "wall", true);
+            setup(2, "water", true);
+            setup(3, "earth", false);
+            setup(4, "tree", true);
+            setup(5, "sand", false);
+    }
+    public void setup(int index, String imageName, boolean collision) {
+
+        UtilityTool uTool =  new UtilityTool();
 
         try {
-
-            tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResource("/tiles/grass.png"));
-
-            tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResource("/tiles/wall.png"));
-            tile[1].collision = true;
-
-            tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResource("/tiles/water.png"));
-            tile[2].collision = true;
-
-            tile[3] = new Tile();
-            tile[3].image = ImageIO.read(getClass().getResource("/tiles/earth.png"));
-
-            tile[4] = new Tile();
-            tile[4].image = ImageIO.read(getClass().getResource("/tiles/tree.png"));
-            tile[4].collision = true;
-
-            tile[5] = new Tile();
-            tile[5].image = ImageIO.read(getClass().getResource("/tiles/sand.png"));
-
-        }catch (Exception e){
+            tile[index] = new Tile();
+            tile[index].image = ImageIO.read(getClass().getResource("/tiles/"+ imageName + ".png"));
+            tile[index].image = uTool.scaleImage(tile[index].image, gp.tileSize, gp.tileSize);
+            tile[index].collision = collision;
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
@@ -105,7 +96,7 @@ public class TileManager {
                 worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
                 worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
 
-                g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+                g2.drawImage(tile[tileNum].image, screenX, screenY, null);
             }
 
             worldCol++;
@@ -115,6 +106,5 @@ public class TileManager {
                 worldRow++;
             }
         }
-
     }
 }
