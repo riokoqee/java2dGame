783ac3a807cdf6d767f0dbaa499014726029ff27
rioko <rioko988@gmail.com>
2025-11-03 00:19:14 +0500

    feat: created npc and interaction, hud, menu, 2 events

---
+++ src/entity/NPC_OldMan.java
@@ -0,0 +1,73 @@
+package entity;
+
+import main.GamePanel;
+
+import java.util.Random;
+
+public class NPC_OldMan extends Entity{
+
+    public NPC_OldMan(GamePanel gp) {
+        super(gp);
+
+        direction = "down";
+        speed = 1;
+
+        getImage();
+        setDialogue();
+    }
+
+    public void getImage() {
+        up1 = setup("/npc/oldman_up_1");
+        up2 = setup("/npc/oldman_up_2");
+        down1 = setup("/npc/oldman_down_1");
+        down2 = setup("/npc/oldman_down_2");
+        left1 = setup("/npc/oldman_left_1");
+        left2 = setup("/npc/oldman_left_2");
+        right1 = setup("/npc/oldman_right_1");
+        right2 = setup("/npc/oldman_right_2");
+    }
+
+    public void setDialogue() {
+
+        dialogues[0] = "wassap fine shyt!";
+        dialogues[1] = "im daryn! aka grand rizzler";
+        dialogues[2] = "do u wanna be minecraft but without \ncraft?";
+        dialogues[3] = "jk, my heart is already taken by nura";
+        dialogues[4] = "dont play with me!";
+    }
+
+    public void setAction() {
+
+        actionLockCounter ++;
+
+        if(actionLockCounter == 120) {
+
+            Random random = new Random();
+            int i = random.nextInt(100)+1;
+
+            if(i <= 25) {
+                direction = "up";
+            }
+
+            if(i > 25 && i <= 50) {
+                direction = "down";
+            }
+
+            if(i > 50 && i <= 75) {
+                direction = "left";
+            }
+
+            if(i > 75 && i <= 100) {
+                direction = "right";
+            }
+
+            actionLockCounter = 0;
+        }
+    }
+
+    public void speak() {
+
+        super.speak();
+
+    }
+}
